0a53c40fcc1d5669411565f22453cdf6c2d58acf
Rahul Gunasekaran <rahulguna10@gmail.com>
2019-11-03 18:33:55 -0500

    stubs

---
+++ smartweb_service/client/client.py
@@ -0,0 +1,22 @@
+from __future__ import print_function
+import logging
+
+import grpc
+
+import smartweb_service.services.stubs.common_pb2
+import smartweb_service.services.stubs.common_pb2_grpc
+
+
+def run():
+    # NOTE(gRPC Python Team): .close() is possible on a channel and should be
+    # used in circumstances in which the with statement does not fit the needs
+    # of the code.
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = common_pb2_grpc.CommonStub(channel)
+        response = stub.GenerateAPIRequest(common_pb2.ApiRequest(name='hey'))
+    print("SmartWeb client received: " + response.message)
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    run()
