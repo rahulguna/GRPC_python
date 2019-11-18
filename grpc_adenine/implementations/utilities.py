0c5bd491e9c276ca3247772d70ef778645b353c2
Nagendra Prasad Prakash <43478877+prasadpgowda@users.noreply.github.com>
2019-11-17 19:52:27 -0500

    Update utilities.py

--- grpc_adenine/implementations/utilities.py
+++ grpc_adenine/implementations/utilities.py
@@ -5,6 +5,7 @@ from grpc_adenine.database.user_api_relation import UserApiRelation
 
 def validate_api_key(api_key):
 	user_api_rel = UserApiRelation.query.filter(UserApiRelation.api_key == api_key).first()
+	#print db.query(exists().where(UserApiRelation.api_key == 'KHBOsth7b3WbOTVzZqGUEhOY8rPreYFM')).scalar()
 	if user_api_rel is None:
 		return False
 	return True
